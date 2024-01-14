8d125bceed2f26835b3da357e8cebfe55a324fb0
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-01-14 01:47:53 +0100

    Add help index generation for main website
    It will generate a table of contents and menu with absolute links to be
    used on the drawpile.net page.

--- reindex.rb
+++ reindex.rb
@@ -53,33 +53,47 @@ end
 
 menu = File.open(File.join(__dir__, '_includes/help/menu.html'), mode: 'w')
 all = File.open(File.join(__dir__, '_includes/help/all.html'), mode: 'w')
+webmenu = File.open(File.join(__dir__, 'website.menu.html'), mode: 'w')
+weball = File.open(File.join(__dir__, 'website.all.html'), mode: 'w')
 all.puts(%q(<ul>))
+weball.puts(%q(<ul>))
 
 for category in categories
   index = File.open(File.join(__dir__, "_includes/help/#{category[:key]}.html"), mode: 'w')
 
   menu.puts(%Q(<p class="menu-label"><a href="/help/#{category[:key]}"{% if page.url == '/help/#{category[:key]}/' %} class="is-active"{% endif %}>#{category[:title]}</a></p>))
   all.puts(%Q(<li><a href="/help/#{category[:key]}">#{category[:title]}</a></li>))
+  webmenu.puts(%Q(<p class="menu-label"><a href="https://docs.drawpile.net/help/#{category[:key]}">#{category[:title]}</a></p>))
+  weball.puts(%Q(<li><a href="https://docs.drawpile.net/help/#{category[:key]}">#{category[:title]}</a></li>))
 
   menu.puts(%q(<ul class="menu-list">))
-  index.puts(%q(<ul>))
   all.puts(%q(<ul>))
+  webmenu.puts(%q(<ul class="menu-list">))
+  weball.puts(%q(<ul>))
+  index.puts(%q(<ul>))
 
   for article in articles[category[:key]].sort_by {|a| a[:title] }
     menu.puts(%Q(  <li><a href="#{article[:link]}"{% if page.url == '#{article[:link]}.html' %} class="is-active"{% endif %}>#{article[:title]}</a></li>))
     all.puts(%Q(  <li><a href="#{article[:link]}">#{article[:title]}</a></li>))
+    webmenu.puts(%Q(  <li><a href="https://docs.drawpile.net#{article[:link]}">#{article[:title]}</a></li>))
+    weball.puts(%Q(  <li><a href="https://docs.drawpile.net#{article[:link]}">#{article[:title]}</a></li>))
     index.puts(%Q(  <li><a href="#{article[:link]}">#{article[:title]}</a></li>))
   end
 
   menu.puts(%q(</ul>))
   all.puts(%q(</ul>))
+  webmenu.puts(%q(</ul>))
+  weball.puts(%q(</ul>))
   index.puts(%q(</ul>))
   index.close()
 end
 
 all.puts(%q(</ul>))
+weball.puts(%q(</ul>))
 all.close()
 menu.close()
+weball.close()
+webmenu.close()
 
 
 faqtoc = File.open(File.join(__dir__, '_includes/help/faqtoc.html'), mode: 'w')
